6b8df44c597aded5cd04970e9cd0137c8d2d3e96
Joseff361 <joseff361@outlook.com>
2020-08-08 13:42:25 -0500

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; //The js file = component
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //The js file = component
+
+
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 
 //Decorator: Is a function that modifies js classes
 @NgModule({ // Allow you spcify some details about the module
@@ -13,7 +17,9 @@ import { AppComponent } from './app.component'; //The js file = component
   ],
   imports: [ //third party modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [], //services
   bootstrap: [AppComponent]
