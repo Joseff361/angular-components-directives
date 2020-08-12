f5b8249af1cdb0d551851e5b80a6df2929fe406e
Joseff361 <joseff361@outlook.com>
2020-08-12 00:16:45 -0500

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,13 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 //Decorator: Is a function that modifies js classes
 @NgModule({ // Allow you spcify some details about the module
   //Metadata
   declarations: [ //components, directives and pipes
-    AppComponent, MenuComponent
+    AppComponent, MenuComponent, DishdetailComponent
   ],
   imports: [ //third party modules
     BrowserModule,
